6b3834b1cf8e14826a1188466e94409a81f41170
kusk <lpunch3corns@naver.com>
2025-04-01 09:08:54 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int IntegerX = 70;
+	int IntegerY = 30;
+
+	cout << IntegerX << " + "<< IntegerY << " = " << IntegerX + IntegerY << endl;
+	cout << IntegerX << " - "<< IntegerY << " = " << IntegerX - IntegerY << endl;
+	cout << IntegerX << " * "<< IntegerY << " = " << IntegerX * IntegerY << endl;
+	cout << IntegerX << " / "<< IntegerY << " = " << IntegerX / IntegerY << endl;
+
+	float FloatX = 1.10;
+	float FloatY = 2.30;
+
+	cout << FloatX << " + " << FloatY << " = " << FloatX + FloatY << endl;
+	cout << FloatX << " - " << FloatY << " = " << FloatX - FloatY << endl;
+	cout << FloatX << " * " << FloatY << " = " << FloatX * FloatY << endl;
+	cout << FloatX << " / " << FloatY << " = " << FloatX / FloatY << endl;
+}
